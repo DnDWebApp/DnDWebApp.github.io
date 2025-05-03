c3278a698b434b9dc2985ae3d884ef8f869d672f
Dennis2008 <dennis.musina2008@gmail.com>
2025-05-03 11:12:15 +0200

    not much

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+createGrid();
 dragElement(document.getElementById("tile"));
-
-
 
 
 
@@ -9,16 +8,22 @@ function createGrid()
 {
 	var gridX = localStorage.getItem("gridX");
 	var gridY = localStorage.getItem("gridY");
+	var grid = document.getElementById("grid");
 	var cont = 0;
 	for(let i = 0; i < gridY; i++)
 	{
 		for(let j = 0; j < gridX; j++)
 		{
 			var emptyTile = document.createElement("img");
+			emptyTile.style.width="100px";
+			emptyTile.style.height="100px";
+			emptyTile.style.padding="0";
+			emptyTile.style.margin="0";
 			emptyTile.src="src/emptyTile.png";
 			emptyTile.id = "emptyTile" + cont;
+			emptyTile.classList.add("emptyTile");
 			cont++;
-			document.getElementById("grid").appendChild(emptyTile);
+			grid.appendChild(emptyTile);
 		}
 		grid.appendChild(document.createElement("br"));
 	}
